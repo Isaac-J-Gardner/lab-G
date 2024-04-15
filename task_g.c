f267a6fe04f527b1fd241e811ac69a9453a7ed45
Isaac Gardner <isaacjamesgardner@hotmail.com>
2024-04-15 13:01:08 +0100

    Add files via upload

---
+++ task_g.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "task_g.h"
+
+int main(void) {
+
+    // Your code below here
+    int scan = 0;
+    double numbers[20];
+    double max_val;
+    FILE *fptr;
+    
+    fptr = fopen("numbers.txt", "r");
+
+    if (fptr != NULL){
+        for(int i = 0; i < 20; i++){
+            scan = fscanf(fptr, "%lf", &numbers[i]);
+            if (scan != 1){
+                return 1;
+            }
+        }
+    }
+
+	find_max_less_than_10(numbers, 20, &max_val);
+
+    printf("max = %lf", max_val);
+
+
+    return 0;
+}
---
+++ task_g.h
@@ -0,0 +1,19 @@
+void find_max_less_than_10(const double a[], const int n, double *max) {
+    
+    // Your code below here
+    double max_val = 0;
+    for(int i = 0; i < n; i++){
+        if(a[i] < 10){
+            if(a[i] > max_val){
+                max_val = a[i];
+            }
+        }
+    }
+    if(max_val == 0){
+        printf("Error!");
+        return;
+    }
+    *max = max_val;
+    return;
+    
+}
